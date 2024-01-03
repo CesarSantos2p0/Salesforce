86a66fae82c5f07cab45a690fd80c95ef4fbf4ff
Paulo Cesar Santos Filho <111451739+CesarSantos2p0@users.noreply.github.com>
2024-01-03 00:13:33 -0300

    botão de editar e de criar produto.

--- force-app/main/default/lwc/produto/produto.js
+++ force-app/main/default/lwc/produto/produto.js
@@ -1,11 +1,12 @@
 import { LightningElement, api } from 'lwc';
 import apagarRegistroProduto from '@salesforce/apex/GerenciadorProdutoController.apagarRegistroProduto';
 import LightningConfirm from 'lightning/confirm';
-import { RefreshEvent } from "lightning/refresh";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class Produto extends LightningElement {
+export default class Produto extends NavigationMixin(LightningElement) {
     @api produto;
 
+    //Essa função manda uma mensagem de confirmação para o usuario e se confirmada ela apaga p registro e atualiza o componente
     async apagarProduto() {
         const result = await LightningConfirm.open({
             message: 'Tem certeza que deseja apagar o produto?',
@@ -16,6 +17,17 @@ export default class Produto extends LightningElement {
                 this.dispatchEvent(new CustomEvent('atualizar'));
             });
         }
+    }
+
+    //Essa função abre a parte de editar o produto após o botão Editar é clicado
+    editarProduto(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.produto.Id,
+                actionName: 'edit',
+            },
+        });
     }
 
 }
